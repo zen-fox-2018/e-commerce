c02502497324d24332acbdc35f1b4232dcfca234
Veneciac <veneciacalista00@gmail.com>
2019-02-01 12:47:01 +0700

    test

--- client/src/main.js
+++ client/src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
-
-Vue.use(BootstrapVue)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
